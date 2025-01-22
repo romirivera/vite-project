f4edf786820e74c12595def70c677034ea4197b5
Romina Rivera <romina.rivera@genstudents.org>
2025-01-22 19:19:51 -0300

    primer commit

---
+++ src/contador.jsx
@@ -0,0 +1,26 @@
+//useState ---> manejar estado en un coomponente
+
+import { useState } from 'react';
+
+function Contador() {
+  //codico js
+  const [contador, setContador] = useState(0);
+  //    estado    fn update el estado    valor inicial del estado
+
+  if (contador < 0) {
+    setContador(0);
+  }
+
+  return (
+    //codigo UI
+
+    <div>
+      <h1>Contador:{contador}</h1>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+      <button onClick={() => setContador(contador - 1)}>Decrementar</button>
+      <button onClick={() => setContador(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default Contador;
